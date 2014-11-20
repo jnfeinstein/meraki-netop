3f6875c74e2aab12af094b793e77ba00710c4df7
joelf <joelf@meraki.net>
2014-11-20 15:29:05 -0800

    netop

--- server.go
+++ server.go
@@ -1,68 +1,18 @@
 package main
 
 import (
-	"fmt"
-	"github.com/codegangsta/martini-contrib/binding"
 	"github.com/go-martini/martini"
 	"github.com/martini-contrib/render"
-	. "goboilerplate/app/models"
-	"goboilerplate/config"
-	. "goboilerplate/db"
-	"html/template"
 	"net/http"
-	"os"
-	"runtime"
-	"strconv"
 )
 
 func main() {
-	runtime.GOMAXPROCS(runtime.NumCPU())
-
 	m := martini.Classic()
 
-	config.Initialize(m)
+	m.Use(render.Renderer())
 
-	db, err := NewDatabaseConnection()
-	if err != nil {
-		fmt.Printf("Error initializing database: %s\n", err.Error())
-		os.Exit(1)
-	}
-
-	m.Use(render.Renderer(render.Options{
-		Funcs: []template.FuncMap{
-			{
-				"heroku": func() bool {
-					return config.Heroku
-				},
-			},
-		},
-		Layout: "app",
-	}))
-
-	m.Get("/", func(r render.Render) {
-		r.HTML(200, "index", nil)
-	})
-
-	m.Post("/cat", binding.Bind(Cat{}), func(w http.ResponseWriter, cat Cat) {
-		if err := db.Create(&cat).Error; err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-		fmt.Fprintf(w, "%d", cat.Id)
-	})
-
-	m.Get("/cat/:id", func(w http.ResponseWriter, r render.Render, p martini.Params) {
-		id, err := strconv.Atoi(p["id"])
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-
-		var cat Cat
-		if err := db.First(&cat, id).Error; err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		r.JSON(200, cat)
+	m.Get("/", func(req *http.Request, r render.Render) {
+		r.HTML(200, "app", req.URL.Query())
 	})
 
 	m.Run()
